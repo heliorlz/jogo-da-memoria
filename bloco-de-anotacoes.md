262792e0119bab103a28c60bbd2b93b423ea8596
Helio Alves <heleralvess@gmail.com>
2019-01-17 08:15:00 -0200

    Explicações e alterações de acordo com a aula 022

---
+++ bloco-de-anotacoes.md
@@ -0,0 +1,3 @@
+Javascript: Evolui anualmente. Para se atualizar, TC39 tem um repositorio no github (TC39);
+
+API do DOM: Acessar recurso que está dentro do Browser, conectar ao navegador utilizando a linguagem Javascript, faz a ligação das pontas. Quem mantém a especificação HTML, CSS e API do DOM são duas organizações: w3c e WHATWG (Browsers);
--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
 const $root = document.querySelector('#root');
-const $memoryCardHeads = document.createElement('article');
-const $memoryCardTails = document.createElement('article');
-const $iconHeads = 
+const $memoryCard = document.createElement('article');
+const $memoryCardFront = document.createElement('article');
+
+const $iconCollab = 
     `<img 
         src="img/icon-collabcode.png" 
         alt="Gueio, mascote da CollabCode" 
         class="icon"
     />`; // transformamos a tag img em string 
     //plus: para podermos quebrar e organizar melhor, usamos templatestring(as aspas)
-const $iconTails = 
-    `<img
-    src="img/icon-js.png"
-    alt="Gueio, mascote da CollabCode"
-    class="icon -js"
-    />`;
 
-$memoryCardHeads.classList.add('memory-card');
-$memoryCardTails.classList.add('-tails');
+const $iconJS = 
+    `<img 
+        src="img/icon-js.png" 
+        alt="Icone do livro de Javascript" 
+        class="icon"
+    />`
 
-// $memoryCardHeads.innerHTML = $icon; 
-
-$root.insertBefore($memoryCardHeads, null); // como não temos nenhuma tag antes do article, devemos colocar null
-$memoryCardHeads.insertAdjacentHTML("afterbegin", $iconHeads); // pegando o caminho do icon e adicionando como html ao memorycard
+$memoryCard.classList.add('memory-card');
+$root.insertBefore($memoryCard, null); // como não temos nenhuma tag antes do article, devemos colocar null
+// $memoryCard.innerHTML = $icon; 
+$memoryCard.insertAdjacentHTML("afterbegin", $iconCollab); // pegando o caminho do icon e adicionando como html ao memorycard
 // insertAdjacentHTML inseri HTML dinamicamente em nosso documento, pegando como parametros a posição e a string a ser inserida
-$root.insertBefore($memoryCardTails, null);
-$memoryCardTails.insertAdjacentHTML("afterbegin", $iconTails);
+
+$memoryCardFront.classList.add('memory-card');
+$memoryCardFront.classList.add('-front');
+$root.insertBefore($memoryCardFront, null);
+$memoryCardFront.insertAdjacentHTML("afterbegin", $iconJS);
+
+
