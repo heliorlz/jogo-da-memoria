ba500886c9271c3aa47c6c5213bdcb645b3754b5
Helio Alves <heleralvess@gmail.com>
2019-01-25 07:10:53 -0200

    desafio: refatorado seguindo os videos da aula 26 + memory-card style sendo chamado pelo arquivo js

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,12 +1,52 @@
 // const numberCards = 9; /* numero de cards - o card front */
 
 // JSON Javascript Object Notation
-// utilizando object destructuring na function
-const createMemoryCard = card => {
+// utilizando object destructuring na function, passando diretamente como parametro
+const createMemoryCard = ({ nameClass, src, alt }) => {
+    const $head = document.querySelector('head');
+    const $style = document.createElement('style');
 
-const { nameClass, src, alt } = card;
-
-return ` 
+    $style.textContent = `
+    .memory-card {
+        width: 130px;
+        height: 130px;
+        border-radius: 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+        background-color: #f25a70;
+        position: relative;
+        margin-bottom: 15px;
+        cursor: pointer;
+      }
+      
+      .memory-card.-front {
+        background-color: #fff;
+      }
+      
+      .memory-card.-front::before {
+        content: "";
+        width: 95px;
+        height: 95px;
+        background-color: #d4d4d4;
+        border-radius: 50%;
+        position: absolute;
+      }
+      
+      .memory-card > .icon {
+        width: 100px;
+        height: 100px;
+      }
+      
+      .memory-card.-front > .icon {
+        position: absolute;
+        transform: translateY(-12px);
+      }
+    `
+    $head.insertBefore($style, null);
+    
+    return ` 
     <article class="memory-card ${nameClass}">
         <img 
             src="${src}" 
@@ -16,9 +56,8 @@ return `
         />
     </article>
     `;
-};
-
-const handleClick = () => console.log('foi');
+}
+const handleClick = () => console.log("foi");
 
 // for (i = 0; i < numberCards; i++) {
 //     const $memoryCard = document.createElement('article');
--- src/objects/cards-wrapper/index.js
+++ src/objects/cards-wrapper/index.js
@@ -2,5 +2,22 @@ function createCardsWrapper() {
   const $cardsWrapper = document.createElement("section");
   $cardsWrapper.classList.add("cards-wrapper");
 
+  const $head = document.querySelector('head'); // pegando a tag head
+  const $style = document.createElement('style'); // criando a tag style
+
+  $style.textContent = `
+  .cards-wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    padding-top: 10px;
+  }
+
+  .cards-wrapper > .memory-card {
+    margin-bottom: 10px;
+  }
+  `
+  $head.insertBefore($style, null); // adicionando a tag style e seu conteudo dentro da tag head
+
   return $cardsWrapper;
 }
