a197f8deaddc1ad1f7ce334b02df5d9100f74f5b
Helio Alves <heleralvess@gmail.com>
2019-02-13 06:52:40 -0200

    alteracoes feitas no video 2 da aula 32 - expondo a funcao de criacao do memoryCard para fora da IIFE

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,97 +1,95 @@
-const memoryCard = () => {
-  const $head = document.querySelector("head");
-  const $style = document.createElement("style");
+const memoryCard = (function() {
+  // me falhou entender o porquê de cardActs.handleClick na inserção do HTML acima, seguindo a live
+  function memoryCard() {
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
 
-  $style.textContent = `
-      .memory-card {
-        width: 130px;
-        height: 130px;
-        position: relative;
-      }
+    $style.textContent = `
+        .memory-card {
+          width: 130px;
+          height: 130px;
+          position: relative;
+        }
+    
+        .memory-card .card {
+          width: 100%;
+          height: 100%;
+          border-radius: 30px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+          background-color: #f25a70;
+          position: relative;
+          margin-bottom: 15px;
+          cursor: pointer;
+          position: absolute;
+        }
   
-      .memory-card .card {
-        width: 100%;
-        height: 100%;
-        border-radius: 30px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
-        background-color: #f25a70;
-        position: relative;
-        margin-bottom: 15px;
-        cursor: pointer;
-        position: absolute;
-      }
-
-      .memory-card.-active .card,
-      .memory-card.-score .card {
-        display: none;
-      }
-
-      .memory-card.-active .card.-front,
-      .memory-card.-score .card.-front {
-        display: flex;
-      }
-      
-     .memory-card .card.-front::before {
-        content: "";
-        width: 95px;
-        height: 95px;
-        background-color: #d4d4d4;
-        border-radius: 50%;
-        position: absolute;
-      }
-      
-     .memory-card .card > .icon {
-        width: 100px;
-        height: 100px;
-      }
-
-     .memory-card .card.-front {
-        background-color: #fff;
-      }
+        .memory-card.-active .card,
+        .memory-card.-score .card {
+          display: none;
+        }
+  
+        .memory-card.-active .card.-front,
+        .memory-card.-score .card.-front {
+          display: flex;
+        }
         
-     .memory-card .card.-front > .icon {
-        position: absolute;
-        transform: translateY(-12px);
-      }
+       .memory-card .card.-front::before {
+          content: "";
+          width: 95px;
+          height: 95px;
+          background-color: #d4d4d4;
+          border-radius: 50%;
+          position: absolute;
+        }
+        
+       .memory-card .card > .icon {
+          width: 100px;
+          height: 100px;
+        }
+  
+       .memory-card .card.-front {
+          background-color: #fff;
+        }
+          
+       .memory-card .card.-front > .icon {
+          position: absolute;
+          transform: translateY(-12px);
+        }
+  
+  
+      `;
+    $head.insertBefore($style, null);
 
-
-    `;
-  $head.insertBefore($style, null);
-
-  return ({ src, alt, dataIcone }) => ` 
-    <div class="memory-card" data-icone="${dataIcone}" onclick="cardActs.handleClick(this)">
-      <article class="card -front">
-        <img 
-            src="${src}" 
-            alt="${alt}" 
+    return ({ src, alt, dataIcone }) => ` 
+      <div class="memory-card" data-icone="${dataIcone}" onclick="cardActs.handleClick(this)">
+        <article class="card -front">
+          <img 
+              src="${src}" 
+              alt="${alt}" 
+              class="icon"
+              
+          />
+        </article>
+        <article class="card">
+          <img 
+            src="img/icon-collabcode.png" 
+            alt="Mascote collabcode" 
             class="icon"
-            
-        />
-      </article>
-      <article class="card">
-        <img 
-          src="img/icon-collabcode.png" 
-          alt="Mascote collabcode" 
-          class="icon"
-        />    
-      </'article>
-    </div>
-    `;
-};
+          />    
+        </'article>
+      </div>
+      `;
+  }
 
-const cardActs = (function() { // me falhou entender o porquê de cardActs.handleClick na inserção do HTML acima, seguindo a live
-  return {
-    handleClick: function($component) {
-      if (!$component.classList.contains("-active")) {
-        activeMemoryCard($component);
-        checkForMatch();
-      }
+  const handleClick = $component => {
+    if (!$component.classList.contains("-active")) {
+      activeMemoryCard($component);
+      checkForMatch();
     }
   };
-
   function activeMemoryCard($component) {
     if (store.qttActiveMemoryCard < 2) {
       $component.classList.add("-active");
@@ -137,4 +135,6 @@ const cardActs = (function() { // me falhou entender o porquê de cardActs.handl
       });
     }, 1500);
   }
+
+  return memoryCard; // return da IIFE que expoe o memory-card para fora
 })();
