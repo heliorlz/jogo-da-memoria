d2f7d79e86e9f13a2655af14e727823876aa0f2e
Helio Alves <heleralvess@gmail.com>
2019-03-13 07:51:36 -0300

    alteracoes feitas no video 5 da aula 39 - usando modificador active com condicional diretamente no css, sem a class

--- src/components/flat-button/index.js
+++ src/components/flat-button/index.js
@@ -1,35 +1,30 @@
 const flatButton = (function() {
   const module = {};
 
-  module._style = () => {
+  module._style = active => {
     const $head = document.querySelector("head");
     const $style = document.createElement("style");
 
     $style.textContent = `
       .flat-button {
-          background-color: #eae6da;
-          color: #fffcee;
+          background-color: ${active ? "#f25a70" : "#eae6da"};
+          color: ${active ? "#fff" : "#fffcee"};
           font-size: 24px;
           font-weight: bold;
           width: 50%;
           height: 176px;
           text-transform: uppercase;
       }
-
-      .flat-button.-active {
-          background-color: #f25a70;
-          color: #fff; 
-      }
     `;
 
     $head.insertBefore($style, null);
   };
 
-  module.render = (content = "", variation = "") => {
-    module._style();
+  module.render = (content = "", active = false) => {
+    module._style(active);
 
     return `
-      <button class="flat-button ${variation}">${content}</button>          
+      <button class="flat-button">${content}</button>          
     `;
   };
 
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -2,7 +2,7 @@
   const $root = document.querySelector("#root");
 
   const $loginButton = flatButton.render("Log in");
-  const $signupButton = flatButton.render("Sign up", "-active");
+  const $signupButton = flatButton.render("Sign up", true);
 
   //inserting HTML content
   $root.insertAdjacentHTML("beforeend", $loginButton);
