2f41d3cb093386184633109ac816e1139c959dae
Helio Alves <heleralvess@gmail.com>
2019-03-13 07:25:39 -0300

    alteracoes feitas no video 4 da aula 39 - modificando flat-button atraves do modificador -active pelo css

--- src/components/flat-button/index.js
+++ src/components/flat-button/index.js
@@ -6,37 +6,30 @@ const flatButton = (function() {
     const $style = document.createElement("style");
 
     $style.textContent = `
-      body {
-          display: block;
-      }    
-
       .flat-button {
           background-color: #eae6da;
           color: #fffcee;
           font-size: 24px;
           font-weight: bold;
-          width: 186px;
+          width: 50%;
           height: 176px;
           text-transform: uppercase;
       }
 
-      .-login {
-          background-color: #eae6da; 
-      }
-
-      .-signup {
+      .flat-button.-active {
           background-color: #f25a70;
+          color: #fff; 
       }
     `;
 
     $head.insertBefore($style, null);
   };
 
-  module.render = (content, background) => {
+  module.render = (content = "", variation = "") => {
     module._style();
 
     return `
-      <button class="flat-button ${background}">${content}</button>          
+      <button class="flat-button ${variation}">${content}</button>          
     `;
   };
 
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,10 +1,10 @@
 (function() {
   const $root = document.querySelector("#root");
 
-  const $flatButton = flatButton.render("Log in", "-login");
-  const $flatButtonSignUp = flatButton.render("Sign up", "-signup");
+  const $loginButton = flatButton.render("Log in");
+  const $signupButton = flatButton.render("Sign up", "-active");
 
   //inserting HTML content
-  $root.insertAdjacentHTML("afterbegin", $flatButton);
-  $root.insertAdjacentHTML("beforeend", $flatButtonSignUp);
+  $root.insertAdjacentHTML("beforeend", $loginButton);
+  $root.insertAdjacentHTML("beforeend", $signupButton);
 })();
