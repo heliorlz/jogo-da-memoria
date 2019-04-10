9098a02100e3e9f08aaec1d7027f2eeb4a6f5b74
Helio Alves <heleralvess@gmail.com>
2019-04-10 09:42:02 -0300

    desafios basico, intermediario e avancado - adicionado type dos inputs, criada a nova page login, reutilizada estilizacao da page signup quando possivel

---
+++ src/components/form-login/index.js
@@ -0,0 +1,50 @@
+const formLogin = (function() {
+    const module = {};
+  
+    module._style = () => {
+      const $head = document.querySelector("head");
+      const $style = document.createElement("style");
+  
+      $style.textContent = `
+        .form-login {
+            display: flex;
+            flex-direction: column;
+            padding: 0 35px;
+            margin-top: 50px;
+            transform: translateY(-20%);
+        }
+      `;
+      $head.insertBefore($style, null);
+    };
+  
+    module._children = () => {
+      // rendering form login
+      const $labelFormUser = labelForm.render("Username or e-mail");
+      const $inputFormUser = inputForm.render("text", "Username", "");
+  
+      const $labelFormPassword = labelForm.render("Password");
+      const $inputFormPassword = inputForm.render("password", "Password", "******");
+  
+      const $submitButton = submitButton.render("Login");
+  
+      return `
+      ${$labelFormUser}
+      ${$inputFormUser}
+  
+      ${$labelFormPassword}
+      ${$inputFormPassword}
+  
+      ${$submitButton}
+    `;
+    };
+
+    module.render = (...$children) => {
+        module._style();
+    
+        return `<form class="form-login" action="" method="POST ">${module._children()}</form>`;
+      };
+    
+      return {
+        render: module.render
+      };
+    })();
---
+++ src/pages/login/index.js
@@ -0,0 +1,20 @@
+(function() {
+  const $root = document.querySelector("#root");
+
+  const $loginButton = flatButton.render("Log in", true);
+  const $signupButton = flatButton.render("Sign up");
+
+  // const $avatarWrapper = createAvatarWrapper();
+  const $titleGame = titleGame.render("Hello!");
+  const $gueioIcon = gueioIcon.render();
+
+  const $logoWrapper = logoWrapper.render($gueioIcon, $titleGame);
+
+  const $formLogin = formLogin.render();
+
+  // inserting HTML content
+  $root.insertAdjacentHTML("beforeend", $loginButton);
+  $root.insertAdjacentHTML("beforeend", $signupButton);
+  $root.insertAdjacentHTML("beforeend", $logoWrapper);
+  $root.insertAdjacentHTML("beforeend", $formLogin);
+})();
