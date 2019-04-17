95639c05cd63612c3e6e950fd0a500f54644b150
Helio Alves <heleralvess@gmail.com>
2019-04-17 08:16:16 -0300

    added style of linkCollab

--- src/components/form-login/index.js
+++ src/components/form-login/index.js
@@ -42,9 +42,9 @@ const formLogin = (function() {
       ${$labelFormPassword}
       ${$inputFormPassword}
   
-      ${$submitButton}
-
       ${$linkCollab}
+
+      ${$submitButton}
     `;
   };
 
--- src/objects/link-collab/index.js
+++ src/objects/link-collab/index.js
@@ -1,12 +1,30 @@
 const linkCollab = (function() {
   const module = {};
 
-  module._style = () => {};
+  module._style = () => {
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+
+    $style.textContent = `
+      .link-collab {
+        display: block;
+        text-decoration: none;
+        color: #3a4042;
+        font-size: 14px;
+        opacity: 0.7;
+        margin-top: 20px;
+        margin-bottom: 40px;
+        text-align: right;    
+      }
+    `;
+
+    $head.insertAdjacentElement("beforeend", $style);
+  };
 
   module.render = ({ href, content }) => {
     module._style();
 
-    return `<a href="${href}">${content}</a> `;
+    return `<a class="link-collab" href="${href}">${content}</a> `;
   };
 
   return {
