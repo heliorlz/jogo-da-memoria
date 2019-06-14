2fe22fdc59dcac61b405105a332cf87fb017f0df
Helio Alves <heleralvess@gmail.com>
2019-06-14 08:45:36 -0300

    Criado o object eye-collabcode

---
+++ src/objects/eye-collabcode/index.js
@@ -0,0 +1,14 @@
+const eyeCollabCode = (function() {
+  const module = {};
+
+  module._style = () => {};
+
+  module.render = () => {
+    // importante criar como texto, pois a imagem do olho não representa de fato algo, para acessibilidade
+    `<label>Mostrar Senha</label>`;
+  };
+
+  return {
+    render: module.render
+  };
+})();
