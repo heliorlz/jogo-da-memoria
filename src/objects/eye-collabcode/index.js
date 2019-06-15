9b7d1645e730ec4da117a5a20ccf7820b60a85cd
Helio Alves <heleralvess@gmail.com>
2019-06-15 15:06:54 -0300

    Esconder o texto e adicionar a imagem eye.png atraves do CSS

--- src/objects/eye-collabcode/index.js
+++ src/objects/eye-collabcode/index.js
@@ -1,11 +1,29 @@
 const eyeCollabCode = (function() {
   const module = {};
 
-  module._style = () => {};
+  module._style = () => {
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+
+    $style.textContent = `
+      .eye-collabcode {
+        background-image: url(/img/eye.png);
+        background-repeat: no-repeat;
+        background-position: center;
+        width: 24px;
+        height: 15px;
+        text-indent: -9999px; /* sumir com o texto Mostrar Senha */
+      }
+    `;
+
+    $head.insertAdjacentElement("beforeend", $style);
+  };
 
   module.render = () => {
+    module._style();
+
     // importante criar como texto, pois a imagem do olho não representa de fato algo, para acessibilidade
-    return `<label>Mostrar Senha</label>`;
+    return `<label class="eye-collabcode">Mostrar Senha</label>`;
   };
 
   return {
