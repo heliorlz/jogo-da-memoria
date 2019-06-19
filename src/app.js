a9ba80ab3d1b3812c1784bd0bfaf4e01ecd7dde7
Helio Alves <heleralvess@gmail.com>
2019-06-19 09:14:05 -0300

    Deletado comentarios desnecessarios

--- src/app.js
+++ src/app.js
@@ -9,11 +9,4 @@
   paths["#/404"] = page404;
 
   paths[hash] ? paths[hash]() : paths["#/404"]();
-
-  // // caso true, mostra a page em questão, caso false, mostra a page 404
-  // if (paths[hash]) {
-  //   paths[hash]();
-  // } else {
-  //   paths["#/404"]();
-  // }
 })();
